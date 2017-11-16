d359d9d19568f21055f73b99d49b5bef672ca976
Kenji Nagahashi <kenji@archive.org>
2017-11-16 13:34:35 -0800

    Set version to "0.4.0a1"

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='CDX-Writer',
-    version='0.4.0',
+    version='0.4.0a1',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
