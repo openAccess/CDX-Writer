a655882093a34179649d1855fc0147c960f72240
Kenji Nagahashi <kenji@archive.org>
2020-01-16 21:46:12 +0000

    Add test for response line with HTTP/2.0

--- tests/test_responseparser.py
+++ tests/test_responseparser.py
@@ -5,7 +5,8 @@ from cdx_writer.handler import HTTPResponseParser
 
 @pytest.mark.parametrize("statusline,expected", [
     # original HTTPResponse raises UnknownProtocol
-    (b'HTTP/0.0 400 Bad Request', (10, 400, b'Bad Request'))
+    (b'HTTP/0.0 400 Bad Request', (10, 400, b'Bad Request')),
+    (b'HTTP/2.0 404 Not Found', (10, 404, b'Not Found'))
 ])
 def test_read_status_incorrect_but_acceptable(statusline, expected):
     fobj = io.BytesIO(statusline + b'\r\nContent-Length: 0\r\n\r\n')
