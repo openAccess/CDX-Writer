9ae7dccae904098d49933c81e27ec4dd106a652e
Kenji Nagahashi <kenji@archive.org>
2020-04-06 19:16:29 +0000

    Version 0.4.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='CDX-Writer',
-    version='0.4.2',
+    version='0.4.3',
     packages=find_packages(),
     install_requires=[
         'warctools>=4.10.0',
