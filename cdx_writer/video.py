d8ad87ae6ea1dbc706e618a615d3d92ade7a59ce
Bill OConnor <boconnor@archive.org>
2021-10-28 17:12:46 +0000

    Index the video metatfile.

--- cdx_writer/video.py
+++ cdx_writer/video.py
@@ -14,23 +14,11 @@ class VideoMetaHandler(RecordHandler):
     def mime_type(self):
         return self._normalize_content_type('application/json;generator-youtube-dl')
 
-class ArchivedVideoHandler(RecordHandler):
-    @property
-    def original_url(self):
-        return 'http://wayback-metadata.archive.org/video-meta/' + self.safe_url()
-
-    @property
-    def massaged_url(self):
-        return 'org,archive,wayback-metadata)/archived-video/' + self.urlkey(self.safe_url())
-
-    @property
-    def mime_type(self):
-        return self._normalize_content_type('application/json;generator=yt-archiver')
 
 class VideoDispatcher(DefaultDispatcher):
     def dispatch_metadata(self, record, env):
         content_type = record.content_type
 
-        if content_type and content_type.startswith('application/json;generator=yt-archiver'):
-            return ArchivedVideoHandler
+        if content_type and content_type.startswith('application/json;generator-youtube-dl'):
+            return VideoMetaHandler
         return None
